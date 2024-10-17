ecbe50b5a11e10b3aef36d65b27c8f23ee2e6756
Semi Hasaj <semi.hasaj@c3.ai>
2024-10-17 12:16:04 -0700

    init

--- unsloth/pretrain/main.py
+++ unsloth/pretrain/main.py
@@ -127,22 +127,11 @@ def main(args):
     max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
     dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
     load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+    MODEL_NAME = "unsloth/Meta-Llama-3.1-8B"
 
-    # 4bit pre quantized models we support for 4x faster downloading + no OOMs.
-    fourbit_models = [
-        "unsloth/mistral-7b-v0.3-bnb-4bit",      # New Mistral v3 2x faster!
-        "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
-        "unsloth/llama-3-8b-bnb-4bit",           # Llama-3 15 trillion tokens model 2x faster!
-        "unsloth/llama-3-8b-Instruct-bnb-4bit",
-        "unsloth/llama-3-70b-bnb-4bit",
-        "unsloth/Phi-3-mini-4k-instruct",        # Phi-3 2x faster!
-        "unsloth/Phi-3-medium-4k-instruct",
-        "unsloth/mistral-7b-bnb-4bit",
-        "unsloth/gemma-7b-bnb-4bit",             # Gemma 2.2x faster!
-    ] # More models at https://huggingface.co/unsloth
 
     model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "unsloth/Llama-3.2-3B-bnb-4bit", # Choose ANY! eg teknium/OpenHermes-2.5-Mistral-7B
+        model_name = MODEL_NAME, # Choose ANY! eg teknium/OpenHermes-2.5-Mistral-7B
         max_seq_length = max_seq_length,
         dtype = dtype,
         load_in_4bit = load_in_4bit,
@@ -151,93 +140,91 @@ def main(args):
 
     model = FastLanguageModel.get_peft_model(
         model,
-        r = 128, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        r = 64, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
         target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
                         "gate_proj", "up_proj", "down_proj",
-
-                        "embed_tokens", "lm_head",], # Add for continual pretraining
-        lora_alpha = 32,
-        lora_dropout = 0, # Supports any, but = 0 is optimized
+                        # "embed_tokens", "lm_head",
+                        ], # Add for continual pretraining
+        lora_alpha = 16,
+        lora_dropout = 0.05, # Supports any, but = 0 is optimized
         bias = "none",    # Supports any, but = "none" is optimized
         # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
         use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
         random_state = 3407,
-        use_rslora = True,   # We support rank stabilized LoRA
+        # use_rslora = True,   # We support rank stabilized LoRA
         loftq_config = None, # And LoftQ
     
     )
 
-    # Wikipedia provides a title and an article text.
-    # Use https://translate.google.com!
-    _wikipedia_prompt = """Wikipedia Article
-    ### Title: {}
 
-    ### Article:
-    {}"""
-    # becomes:
-    wikipedia_prompt = """위키피디아 기사
-    ### 제목: {}
-
-    ### 기사:
-    {}"""
 
     EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
     def formatting_prompts_func(examples):
-        titles = examples["title"]
-        texts  = examples["text"]
-        outputs = []
-        for title, text in zip(titles, texts):
+        inputs       = examples["inputs"]
+        outputs      = examples["targets"]
+        texts = []
+        for inp, output in zip(inputs, outputs):  
             # Must add EOS_TOKEN, otherwise your generation will go on forever!
-            text = wikipedia_prompt.format(title, text) + EOS_TOKEN
-            outputs.append(text)
-        return { "text" : outputs, }
+            text = output  + EOS_TOKEN
+            texts.append(text)
+        return { "text" : texts, }
     pass
 
+    DATASET_TRAIN_PATH = os.path.join(args.mounted_data_folder, "joined/train")
+    DATASET_VAL_PATH = os.path.join(args.mounted_data_folder, "joined/validation")
 
-    dataset = load_dataset("wikimedia/wikipedia", "20231101.ko", split = "train",)
+    from datasets import load_dataset, load_from_disk
+    dataset_train = load_from_disk(DATASET_TRAIN_PATH)
+    dataset_train = dataset_train.map(formatting_prompts_func, batched = True,)
+    dataset_train = dataset_train.train_test_split(train_size = 0.01)["train"]
 
-    # We select 1% of the data to make training faster!
-    dataset = dataset.train_test_split(train_size = 0.01)["train"]
-    dataset = dataset.train_test_split(train_size = 0.9)
-    train_dataset= dataset["train"]
-    eval_dataset =dataset["test"]
-    train_dataset = train_dataset.map(formatting_prompts_func, batched = True,)
-    eval_dataset = eval_dataset.map(formatting_prompts_func, batched = True,)
+    dataset_val = load_from_disk(DATASET_VAL_PATH)
+    dataset_val = dataset_val.map(formatting_prompts_func, batched = True,)
+    dataset_val = dataset_val.train_test_split(train_size = 0.01)["train"]
 
+
+
+    from trl import SFTTrainer
+    from transformers import TrainingArguments
+    from unsloth import is_bfloat16_supported
+    from unsloth import UnslothTrainer, UnslothTrainingArguments
 
     trainer = UnslothTrainer(
         model = model,
         tokenizer = tokenizer,
-        train_dataset = train_dataset,
-        eval_dataset=eval_dataset,
+        train_dataset = dataset_train,
+        eval_dataset = dataset_val,
         dataset_text_field = "text",
         max_seq_length = max_seq_length,
-        dataset_num_proc = 2,
+        dataset_num_proc = 8,
+        packing = False, # Can make training 5x faster for short sequences.
         callbacks=[MlflowLoggingCallback()],
+
         args = UnslothTrainingArguments(
-            per_device_train_batch_size = 2,
-            gradient_accumulation_steps = 8,
-
-            # Use warmup_ratio and num_train_epochs for longer runs!
-            max_steps = 120,
-            warmup_steps = 10,
-            # warmup_ratio = 0.1,
-            # num_train_epochs = 1,
-
-            # Select a 2 to 10x smaller learning rate for the embedding matrices!
-            learning_rate = learning_rate,
-            embedding_learning_rate = 1e-5,
-
+            per_device_train_batch_size = 8,
+            gradient_accumulation_steps = 2,
+            # warmup_steps = 5, # apply as needed
+            # num_train_epochs = 3, 
+            max_steps = 100, # set this to define steps instead of epochs
+            learning_rate = 1e-4,
+            # embedding_learning_rate = 1e-6, # set this if 'embed_tokens' and 'lm_head' modules were included in target_modules
             fp16 = not is_bfloat16_supported(),
             bf16 = is_bfloat16_supported(),
-            logging_steps = 1,
+            logging_steps = 10,
             optim = "adamw_8bit",
-            weight_decay = 0.01,
+            # weight_decay = 0.01,
             lr_scheduler_type = "linear",
             seed = 3407,
-            output_dir = "outputs",
+            save_steps = 10,
+            do_eval = True,
+            eval_steps = 10,
+            eval_on_start = True,
+            eval_strategy = "steps",
+            output_dir = "shasaj_output_dir",
+            # report_to = "wandb",
         ),
     )
+
 
     #@title Show current memory stats
     gpu_stats = torch.cuda.get_device_properties(0)
